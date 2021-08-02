47ffe818a6f27228c20b05f70ac3da78748ff4fb
Brian Eft <brianeft@gmail.com>
2021-08-02 13:37:57 -0400

    Updated capstone.sql

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -24,10 +24,43 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
 
+CREATE TABLE cities (
+	city_id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	city_name nvarchar(50) NOT NULL
+)
+
+CREATE TABLE agencies (
+	agency_id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	agency_name nvarchar(50) NOT NULL,
+	address nvarchar(64) NOT NULL,
+	address2 nvarchar(64),
+	state nvarchar(30) NOT NULL,
+	city_id int FOREIGN KEY REFERENCES cities(city_id),
+	postal_code nvarchar(20) NOT NULL,
+	agency_description nvarchar(255) NOT NULL,
+)
+
+CREATE TABLE pets (
+	pet_id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	pet_name nvarchar(50) NOT NULL,
+	pet_type nvarchar(50) NOT NULL,
+	pet_breed nvarchar(50) NOT NULL,
+	pet_age int NOT NULL,
+	pet_description nvarchar(255) NOT NULL, 
+	pet_image_url nvarchar(255),
+	agency_id int FOREIGN KEY REFERENCES agencies(agency_id)
+)
+
 --populate default data
 -- user/password
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 --admin/password
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+INSERT INTO cities (city_name) VALUES ('Columbus');
+INSERT INTO agencies (agency_name, address, state, city_id, postal_code, agency_description) VALUES ('Franklin County Dog Shelter & Adoption Center', '4340 Tamarack Blvd', 'Ohio', 1, '43229', 'We provide compassionate care for impounded animals at the shelter, as well as adoption and lost dog services to the community. We also educate the public and teach dog owners to be more responsible citizens.');
+
+INSERT INTO pets (pet_name, pet_type, pet_breed, pet_age, pet_description, pet_image_url, agency_id)
+VALUES ('Bobo', 'Dog', 'GSD', 3, 'A good boy', 'https://pet-uploads.adoptapet.com/d/7/5/552365685.jpg', 1);
 
 GO
