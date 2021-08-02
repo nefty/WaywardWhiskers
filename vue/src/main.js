18af8e1a0f1a05301cba033dd1119ebcb74b5eb1
Brian Eft <brianeft@gmail.com>
2021-08-02 15:59:03 -0400

    install Bootstrap-Vue

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
 
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
