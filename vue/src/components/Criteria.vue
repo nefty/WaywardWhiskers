2759505af249183486c3f5cff01422c2650fc6e2
Courtney Tipps <tippy2.tenor@gmail.com>
2021-08-03 17:01:29 -0400

    Dropdown Menu Update

---
+++ vue/src/components/Criteria.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+      <label>Type:  </label>
+              <select name="type" id="type">
+                  <option value=""></option>
+                  <option value="dog">Dog</option>
+                  <option value="cat">Cat</option>
+              </select>
+      <label>Age Range: </label>
+      <select name="type" id="type">
+                  <option value=""></option>
+                  <option value="baby">&lt;1</option>
+                  <option value="young">1-3</option>
+                  <option value="adult">3-5</option>
+                  <option value="senior">6+</option>
+              </select>
+      <label>Breed: </label>
+      <select name="breed" id="breed">
+      <option v-for="pet in this.$store" v-bind:key="pet.id"> {{pet.breed}} </option>
+      </select>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "Criteria",
+
+    data() {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,15 @@
   <div class="home">
     <h1>Wayward Whiskers</h1>
     <p>Welcome to Wayward Whiskers!</p>
+    <Criteria />
   </div>
 </template>
 
 <script>
+import Criteria from "@/components/Criteria.vue"
+
 export default {
-  name: "home"
+  name: "home",
+  components: {Criteria}
 }
 </script>
