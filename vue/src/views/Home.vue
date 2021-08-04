619780f4fbc70c48ecff9091bfde142481041d89
Dan Goodrich <dgoodrich94@gmail.com>
2021-08-04 12:23:18 -0400

    PetCard started, struggling with collapsable description

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,15 +2,12 @@
   <div class="home">
     <h1>Wayward Whiskers</h1>
     <p>Welcome to Wayward Whiskers!</p>
-    <Criteria />
   </div>
 </template>
 
 <script>
-import Criteria from "@/components/Criteria.vue"
 
 export default {
   name: "home",
-  components: {Criteria}
 }
 </script>
