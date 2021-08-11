feca92efb74a59c6fdc318b9b6b645a26aeec912
Dan Goodrich <dgoodrich94@gmail.com>
2021-08-11 12:43:52 -0400

    Finishing up Pet Details Page

---
+++ vue/src/components/SingleInstanceMap.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-container>
+    <div style="height: 500px; width: 100%">
+      <l-map
+        :zoom="zoom"
+        :center="center"
+        :options="mapOptions"
+        style="height: 80%"
+      >
+        <l-tile-layer :url="url" :attribution="attribution" />
+        <l-marker :lat-lng="[agency.lat, agency.lon]">
+          <l-popup>
+            {{ agency.name }}
+          </l-popup>
+        </l-marker>
+      </l-map>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "single-instance-map",
+
+  data() {
+    return {
+      zoom: 4,
+      center: {},
+      url:
+        "https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2F5d2FyZHdoaXNrZXJzIiwiYSI6ImNrczZ2cmRqeDA1eGkzMnBjZG56ZTNjd20ifQ.gEc2Jf8bIdDFzmmcrdZ4nw",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      mapOptions: {
+        zoomSnap: 0.5,
+      },
+      agency: {},
+    };
+  },
+  created() {
+      this.agency = this.$store.state.activeAgency;
+  },
+};
+</script>
+
+<style>
+</style>
