a58bc3831d8468bdaf8e55a023ce94f3a8dc40e0
Dan Goodrich <dgoodrich94@gmail.com>
2021-08-12 11:25:39 -0400

    Redid collapsible content

--- vue/src/components/PetCard.vue
+++ vue/src/components/PetCard.vue
@@ -1,37 +1,51 @@
 <template>
-    <b-card
-      elevation="2"
-      bg-variant="light"
-      v-bind:title="pet.name"
-      v-bind:img-src="pet.primaryImageUrl"
-      img-alt="Image"
-      tag="article"
-      style="max-width: 20rem"
-      class="mb-2"
-      display="flex"
-      v-if="(this.$store.state.matchedPets.includes(pet) === false && this.$store.state.rejectedPets.includes(pet) === false) || isMatchedPage === true"
-      v-on:mouseover="setActivePet"
+  <b-card
+    elevation="2"
+    bg-variant="light"
+    v-bind:title="pet.name"
+    v-bind:img-src="pet.primaryImageUrl"
+    img-alt="Image"
+    tag="article"
+    style="max-width: 20rem"
+    class="mb-2"
+    display="flex"
+    v-if="
+      (this.$store.state.matchedPets.includes(pet) === false &&
+        this.$store.state.rejectedPets.includes(pet) === false) ||
+      isMatchedPage === true
+    "
+    v-on:mouseover="setActivePet"
+  >
+    <b-collapse id="collapse-1">
+      <b-card-text v-if="pet.ageString !== ''"
+        >Age: {{ pet.ageString }}</b-card-text
+      >
+      <b-card-text v-else>Age: {{ pet.ageGroup }}</b-card-text>
+      <b-card-text>Sex: {{ pet.sex }}</b-card-text>
+
+      <b-card-text>Size: {{ pet.sizeGroup }}</b-card-text>
+      <b-card-text>Breed: {{ pet.breed }}</b-card-text>
+
+      <b-card-text v-if="pet.exerciseNeeds !== ''">Exercise Needs: {{ pet.exerciseNeeds }}</b-card-text>
+      <b-card-text v-show="pet.vocalLevel !== ''"
+        >Vocal Level: {{ pet.vocalLevel }}</b-card-text
+      >
+    </b-collapse>
+    <b-button v-b-toggle.collapse-1 variant="primary" v-show="!isMatchedPage"
+      >About {{ pet.name }}
+    </b-button>
+    <accept-reject-icons-bar v-show="!isMatchedPage" />
+    <router-link
+      v-show="isMatchedPage"
+      :to="{ name: 'pet-details', params: { id: pet.petId } }"
     >
-      <b-collapse id="collapse-1">
-        
-          <p class="card-text">{{ pet.descriptionText }}</p>
-        
-      </b-collapse>
-      <b-button v-b-toggle.collapse-1 variant="primary" v-show="!isMatchedPage"
-        >About {{pet.name}}
-      </b-button>
-      <accept-reject-icons-bar  v-show="!isMatchedPage"/>
-      <router-link v-show="isMatchedPage" :to="{ name: 'pet-details', params: {id: pet.petId} }" >
-        
-      <b-button variant="primary"> View Adoption Details
-      </b-button>
-        
-        </router-link>
-    </b-card>
+      <b-button variant="primary"> View Adoption Details </b-button>
+    </router-link>
+  </b-card>
 </template>
 
 <script>
-import AcceptRejectIconsBar from './AcceptRejectIconsBar.vue';
+import AcceptRejectIconsBar from "./AcceptRejectIconsBar.vue";
 
 export default {
   components: { AcceptRejectIconsBar },
@@ -43,12 +57,10 @@ export default {
     },
   },
   methods: {
-    setActivePet(){
+    setActivePet() {
       this.$store.commit("SET_ACTIVE_PET", this.pet);
     },
   },
-  
-  
 };
 </script>
 
@@ -62,8 +74,6 @@ export default {
   justify-content: center;
   margin: auto;
   display: flex;
-  
-  
 }
 .mb-2 img {
   height: 220px;
@@ -71,5 +81,4 @@ export default {
   margin: 5px;
   pointer-events: none;
 }
-
 </style>
